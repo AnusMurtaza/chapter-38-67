567e9c1bc8ce77570af6f632cabd5ad9330f4d54
AnusMurtaza <anas.aq97@gmail.com>
2020-07-23 20:21:35 +0500

    assignment

---
+++ app.js
@@ -0,0 +1,37 @@
+
+
+function assignment38_42task1() {
+
+};
+
+function assignment38_42task2() {
+
+};
+
+function assignment38_42task3() {
+
+};
+
+
+function assignment38_42task4() {
+
+};
+
+function assignment38_42task5() {
+
+};
+
+function assignment38_42task6() {
+
+};
+
+function assignment38_42task7() {
+};
+
+
+
+
+
+    // ****************************************************ASSIGNMENT # 35-38 ENDS************************************************************
+
+
